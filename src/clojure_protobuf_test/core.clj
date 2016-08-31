49ba271fbcf3f4bcc36ff28c5ee1d9c19b6e8e16
Dan Fornika <dan@fornikascience.com>
2016-08-30 19:08:27 -0700

    First ReadAlignment that compiles

--- src/clojure_protobuf_test/core.clj
+++ src/clojure_protobuf_test/core.clj
@@ -14,36 +14,45 @@
   (def ReadAlignment (protodef ga4gh.Reads$ReadAlignment))
   (def LinearAlignment (protodef ga4gh.Reads$LinearAlignment))
   (def Position (protodef ga4gh.Common$Position))
-  (def Strand (protodef ga4gh.Common$Strand))
   (def CigarUnit (protodef ga4gh.Reads$CigarUnit))
-    
-  (def positive_strand (protobuf Strand "POS_STRAND"))
   
-  (def p (protobuf Position
+  (def p0 (protobuf Position
                    :reference_name "hg38"
                    :position 0
-                   :strand positive_strand))
+                   :strand :POS_STRAND))
+
+  (def p1 (protobuf Position
+                   :reference_name "hg38"
+                   :position 1
+                   :strand :POS_STRAND))
+
+  (def cu (protobuf CigarUnit
+                    :operation :OPERATION_UNSPECIFIED
+                    :operation_length 100))
   
   (def la (protobuf LinearAlignment
-                    :position p
+                    :position p0
                     :mapping_quality 0
-                    :c))
+                    :cigar_unit [cu cu cu]))
   
   (def r (protobuf ReadAlignment
-                   :id 
-                   :read_group_id
-                   :fragment_name
+                   :id "read alignment x"
+                   :read_group_id "read group x"
+                   :fragment_name "fragment x"
                    :improper_placement false
                    :duplicate_fragment false
-                   :number_reads
-                   :fragment_length
-                   :read_number
+                   :number_reads 1
+                   :fragment_length 100
+                   :read_number 0
                    :failed_vendor_quality_checks false
-                   :alignment
+                   :alignment la
                    :secondary_alignment false
                    :supplementary_alignment false
-                   :aligned_sequence
-                   :aligned_quality
-                   :next_mate_position
-                   :info))
+                   :aligned_sequence "CGATGCATGACT"
+                   :aligned_quality [30 30 30 30
+                                     30 28 28 27
+                                     26 22 20 18]
+                   :next_mate_position p1
+                   :info {:somedata [0 1 2]
+                          :someotherdata ["foo" "bar"]}))
 )
